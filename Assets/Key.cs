524fe554771af29c837221fd136dd3915f1c7bd4
zemin xu <zemin.xu@yahoo.com>
2021-01-25 14:55:58 +0100

    before adding controller mesh

---
+++ Assets/Key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("get the key");
+        }
+    }
+}
