93bf86b87133042edd5f65080038e3cc29aa495f
zemin xu <zemin.xu@yahoo.com>
2021-01-30 16:57:18 +0100

    lock done

---
+++ Assets/Lock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public bool hasKey = false;
+
+    public Transform top;
+
+    public void OnKeyGrabbed()
+    {
+        hasKey = true;
+    }
+
+    public void OnLockAttempted()
+    {
+        if (hasKey)
+        {
+            StartCoroutine(OpenBox());
+        }
+        else
+        {
+            // cannot open
+        }
+    }
+
+    private IEnumerator OpenBox()
+    {
+        int i = 0;
+        while (i < 45)
+        {
+            top.Rotate(new Vector3(0, -1, 0));
+            i++;
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
